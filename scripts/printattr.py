6d26f4a8b70bdf54491f5b17d2993e6273f418df
Korneliusz Osmenda <korneliuszo@gmail.com>
2021-04-15 19:07:45 +0200

    Add computer readable parsed struct & small corrections

--- scripts/printattr.py
+++ scripts/printattr.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 import struct
 import itertools
+import copy
 
-def CISTPL_DEVICE(t):
+def CISTPL_DEVICE(t,pdict):
     ret = "CISTPL_DEVICE " + str(t) + "\n"
     i=2
-    ret += DEVICE_INFO(t[i:])
+    leaf = {}
+    ret += DEVICE_INFO(t[i:],leaf)
+    pdict["DEVICE"]=leaf
     return ret
 
-def DEVICE_INFO(t):
+def DEVICE_INFO(t,leaf):
     ret = ""
     i = 0
+    leaf["DeviceInfo"]=[]
     while t[i] != 0xff:
+        leaf2={}
         device_type = {
                 0 : "DTYPE_NULL",
                 1 : "DTYPE_ROM",
@@ -25,14 +30,17 @@ def DEVICE_INFO(t):
                 0xe: "DTYPE_EXTEND",
             }
         device_speed = {
-                0 : "DSPEED_NULL",
-                1 : "DSPEED_250NS",
-                2 : "DSPEED_200NS",
-                3 : "DSPEED_150NS",
-                4 : "DSPEED_100NS",
-                7 : "DSPEED_EXT",
+                0 : ("DSPEED_NULL",None),
+                1 : ("DSPEED_250NS",250e-9),
+                2 : ("DSPEED_200NS",200e-9),
+                3 : ("DSPEED_150NS",150e-9),
+                4 : ("DSPEED_100NS",100e-9),
+                7 : ("DSPEED_EXT",None),
             }
-        ret += " " + device_type[t[i]>>4] + (" WEN " if t[i]&0x08 else " WP ") + device_speed[t[i]&0x7] +"\n"
+        ret += " " + device_type[t[i]>>4] + (" WEN " if t[i]&0x08 else " WP ") + device_speed[t[i]&0x7][0] +"\n"
+        leaf2["Dtype"] = device_type[t[i]>>4]
+        leaf2["WEN"] = bool(t[i]&0x08)
+        leaf2["Speed"] = device_speed[t[i]&0x7][1]
         off=0
         if t[i]&0x7 == 0x7:
             off+=1
@@ -63,9 +71,10 @@ def DEVICE_INFO(t):
                     6: 1e-3,
                     7: 10e-3,
                 }
-            ret += "  EXTENDED DEV_SPEED: " + "{:.1e}\n".format(
-                    device_speed_mantisa[(t[i+off]&0x78)>>4] *
-                    device_speed_exponent[(t[i+off]&0x07)])
+            speed= device_speed_mantisa[(t[i+off]&0x78)>>4] * device_speed_exponent[(t[i+off]&0x07)]
+            ret += "  EXTENDED DEV_SPEED: " + "{:.1e}\n".format(speed)
+            leaf2["Speed"] = speed
+
             while t[i+off] &0x80:
                 ret += "  EXTENDED DEV_SPEED: " + hex(t[i+off])+"\n"
                 off+=1
@@ -77,51 +86,72 @@ def DEVICE_INFO(t):
                 ret += "  EXTENDED DEV_ID: " + hex(t[i+off])+"\n"
                 off+=1
         i+=off+1
+        leaf["DeviceInfo"].append(leaf2)
     return ret
 
-def CISTPL_DEVICE_OC(t):
+def CISTPL_DEVICE_OC(t,pdict):
     ret = "CISTPL_DEVICE_OC " + str(t) + "\n"
     i=2
     VCC_DICT = { 0: "5V", 1:"3.3V",2: "X.XV", 3:"Y.YV"}
-
+    leaf = {}
+    leaf["OtherCondidions"] = []
     while True:
         ret+=" VCC " + VCC_DICT[(t[i]>>1)&0x3] + (" MWAIT" if t[i]&0x01 else " NOMWAIT") + "\n"
+        leaf["OtherCondidions"].append({
+                "VCC" :  VCC_DICT[(t[i]>>1)&0x3],
+                "MWAIT" : bool(t[i]&0x01),
+            }) 
         i+=1
         if not t[i-1]&0x80:
             break
-    ret+=DEVICE_INFO(t[i:])
+    ret+=DEVICE_INFO(t[i:],leaf)
+    pdict["DEVICE_OC"]=leaf    
     return ret
 
-def CISTPL_JEDEC_C(t):
+def CISTPL_JEDEC_C(t,pdict):
     ret = "CISTPL_DEVICE_JEDEC_C " + str(t) + "\n"
     i=2
+    leaf=[]
     for mfg,cid in zip(t[i::2], t[i+1::2]):
         ret+=" MFG: " + hex(mfg) + " ID: " + hex(cid) + "\n"
+        leaf.append({
+            "MFG" : mfg,
+            "ID" : cid,
+        })
+    pdict["JEDEC"] = leaf
     return ret
 
-def CISTPL_MANFID(t):
+def CISTPL_MANFID(t,pdict):
+    pdict.setdefault("MANFID",[])
     ret = "CISTPL_MANFID " + str(t) + "\n"
     manf,card = struct.unpack("<HH",bytes(t[2:]))
     ret += " MANF: " + hex(manf) + " CARD: " + hex(card) +"\n"
+    pdict["MANFID"].append({ "MANF" : manf, "CARD" : card})
     return ret
 
 
-def CISTPL_VERS_1(t):
+def CISTPL_VERS_1(t,pdict):
+    leaf = {}
     ret = "CISTPL_VERS_1 " + str(t) + "\n"
     ret += " MAJOR: " + str(t[2]) + " MINOR: " + str(t[3]) + "\n"
+    leaf["MAJOR"] = t[2]
+    leaf["MINOR"] = t[3]
     i = iter(t[4:])
-    for info in ("PRODUCT: ", "NAME: ", "ADD1: ", "ADD2: "):
+    for info in ("PRODUCT", "NAME", "ADD1", "ADD2"):
         st=next(i)
         if st == 0xff:
+            pdict["VERS"] = leaf
             return ret
         if st == 0:
             string = ""
         else:
             string=chr(st) + bytes(itertools.takewhile(int(0).__ne__,i)).decode("ascii")
-        ret+=" " + info + string + "\n"
+        ret+=" " + info + ": " + string + "\n"
+        leaf[info] = string
+    pdict["VERS"] = leaf
     return ret
 
-def CISTPL_FUNCID(t):
+def CISTPL_FUNCID(t,pdict):
     ret = "CISTPL_FUNCID " + str(t) + "\n"
     functiondict = {
             0x00: "Multi-Function",
@@ -141,25 +171,37 @@ def CISTPL_FUNCID(t):
         }
     ret += " FUNCTION: " + functiondict[t[2]] + "\n"
     ret += " " + ("ROM" if t[3]&0x02 else "NOROM") + " " + ("POST" if t[3]&0x01 else "NOPOST") + "\n"
+    pdict["FUNCID"] = {}
+    pdict["FUNCID"]["FUNCTION"] = functiondict[t[2]]
+    pdict["FUNCID"]["ROM"] =  bool(t[3]&0x02)
+    pdict["FUNCID"]["POST"] =  bool(t[3]&0x01)
     return ret
 
-def CISTPL_FUNCE(t):
-    def FUNCE_ATA(t):
+def CISTPL_FUNCE(t,pdict):
+    pdict.setdefault("FUNCE",[])
+    def FUNCE_ATA(t,leaf):
         ret = " FUNCE_ATA\n"
+        leaf["TYPE"] = "ATA"
         return ret
-    def FUNCE_ATA2(t):
+    def FUNCE_ATA2(t,leaf):
         ret = " FUNCE_ATA2\n"
-        ret += "  V=" + str((t[3]&0x03)>>0) + "\n"
-        ret += "  S=" + str((t[3]&0x04)>>2) + "\n"
-        ret += "  U=" + str((t[3]&0x08)>>3) + "\n"
-        ret += "  D=" + str((t[3]&0x10)>>4) + "\n"
-        ret += "  P0=" + str((t[4]&0x01)>>0) + "\n"
-        ret += "  P1=" + str((t[4]&0x02)>>1) + "\n"
-        ret += "  P2=" + str((t[4]&0x04)>>2) + "\n"
-        ret += "  P3=" + str((t[4]&0x08)>>3) + "\n"
-        ret += "  N=" + str((t[4]&0x10)>>4) + "\n"
-        ret += "  E=" + str((t[4]&0x20)>>5) + "\n"
-        ret += "  I=" + str((t[4]&0x40)>>6) + "\n"
+        leaf["TYPE"] = "ATA2"
+        bitdict = {
+            "V"  : bool((t[3]&0x03)>>0),
+            "S"  : bool((t[3]&0x04)>>2),
+            "U"  : bool((t[3]&0x08)>>3),
+            "D"  : bool((t[3]&0x10)>>4),
+            "P0" : bool((t[4]&0x01)>>0),
+            "P1" : bool((t[4]&0x02)>>1),
+            "P2" : bool((t[4]&0x04)>>2),
+            "P3" : bool((t[4]&0x08)>>3),
+            "N"  : bool((t[4]&0x10)>>4),
+            "E"  : bool((t[4]&0x20)>>5),
+            "I"  : bool((t[4]&0x40)>>6),
+        }
+        for key,val in bitdict.items():
+            ret += "  " + key + "=" + str(val) + "\n"
+            leaf[key] = val
         return ret
 
     ret = "CISTPL_FUNCE " + str(t) + "\n"
@@ -168,32 +210,45 @@ def CISTPL_FUNCE(t):
             0x02 : FUNCE_ATA2,
         }
     if t[2] in knownfunc.keys():
-        return ret + knownfunc[t[2]](t)
+        leaf = {}
+        ret += knownfunc[t[2]](t,leaf)
+        pdict["FUNCE"].append(leaf)
+        return ret
     else:
         return ret + " UNKNOWN"
 
-def CISTPL_CONFIG(t):
+def CISTPL_CONFIG(t,pdict):
     ret = "CISTPL_CONFIG " + str(t) + "\n"
     tpcc_rmsz = ((t[2] & 0x3C)>>2) + 1
     tpcc_rasz = ((t[2] & 0x03)>>0) + 1
     ret += " TPCC_LAST: " + str(t[3]) + "\n"
     n=0
-    TPC_RADR = 0
+    TPCC_RADR = 0
     for val in t[4:4+tpcc_rasz]:
-        TPC_RADR |= val << n
+        TPCC_RADR |= val << n
         n+=8
-    ret += " TPC_RADR: " + hex(TPC_RADR) + "\n"
+    ret += " TPCC_RADR: " + hex(TPCC_RADR) + "\n"
     n=0
-    TPC_RMSK = 0
+    TPCC_RMSK = 0
     for val in t[4+tpcc_rasz:4+tpcc_rasz+tpcc_rmsz]:
-        TPC_RMSK |= val << n
+        TPCC_RMSK |= val << n
         n+=8
-    ret += " TPC_RMSK: " + hex(TPC_RMSK) + "\n"
+    ret += " TPCC_RMSK: " + hex(TPCC_RMSK) + "\n"
+    pdict["CONFIG"] = {
+            "TPCC_LAST" : t[3],
+            "TPCC_RADR" : TPCC_RADR,
+            "TPCC_RMSK" : TPCC_RMSK,
+        }
     return ret
 
-def CISTPL_CFTABLE_ENTRY(t):
+def CISTPL_CFTABLE_ENTRY(t,pdict):
     ret = "CISTPL_CFTABLE_ENTRY " + str(t) + "\n"
     ret += " CONF_ENTRY_NUMBER: " + str(t[2]&0x3f) + (" (DEFAULT)\n" if t[2]&0x40 else "\n")
+    idx = t[2]&0x3f
+    pdict.setdefault("CFTABLE",{})
+    pdict["CFTABLE"].setdefault(idx,[])
+    leaf = copy.deepcopy(pdict["CFTABLE"][idx][-1]) if len(pdict["CFTABLE"][idx]) else {}
+    leaf["DEFAULT"] = bool(t[2]&0x40)
     reg = 3
     if t[2]&0x80:
         ift = {
@@ -205,10 +260,16 @@ def CISTPL_CFTABLE_ENTRY(t):
                 7 : "Custom interface 3",
             }
         ret += " " + ift[t[reg]&0x0f] + "\n"
-        ret += " " + "BVDs Active=" + str(bool(t[reg]&0x10)) + "\n"
-        ret += " " + "WP Active=" + str(bool(t[reg]&0x20)) + "\n"
-        ret += " " + "READY Active=" + str(bool(t[reg]&0x40)) + "\n"
-        ret += " " + "M Wait Active=" + str(bool(t[reg]&0x80)) + "\n"
+        leaf["IF_TYPE"] = ift[t[reg]&0x0f]
+        tpce_if_fields = {
+                "BVDs" : bool(t[reg]&0x10),
+                "WP"  : bool(t[reg]&0x20),
+                "READY" : bool(t[reg]&0x40),
+                "M Wait" : bool(t[reg]&0x80),
+            }
+        for key,val in tpce_if_fields.items():
+            ret += " " + key + " Active=" + str(val) + "\n"
+            leaf["IF_"+key] = val
         reg+=1
 
     TPCE_FS = t[reg]
@@ -220,6 +281,7 @@ def CISTPL_CFTABLE_ENTRY(t):
             3 : ["Vcc","Vpp1","Vpp2"],
         }
     for pd in pd_dict[TPCE_FS&0x03>>0]:
+        leaf.setdefault(pd,{})
         TPCE_PD = t[reg]
         reg+=1
         ret += " " + pd + "\n"
@@ -264,29 +326,38 @@ def CISTPL_CFTABLE_ENTRY(t):
         if TPCE_PD & 0x01:
             val,reg = to_int(t,reg,100)
             ret += "  Norm V: " + val + "V\n"
+            leaf[pd]["V_Norm"] = val
         if TPCE_PD & 0x02:
             val,reg = to_int(t,reg,100)
             ret += "  Min V: " + val + "V\n"
+            leaf[pd]["V_Min"] = val
         if TPCE_PD & 0x04:
             val,reg = to_int(t,reg,100)
             ret += "  Max V: " + val + "V\n"
+            leaf[pd]["V_Max"] = val
         if TPCE_PD & 0x08:
             val,reg = to_int(t,reg,1)
             ret += "  Static I: " + val + "A\n"
+            leaf[pd]["I_Static"] = val
         if TPCE_PD & 0x10:
             val,reg = to_int(t,reg,1)
             ret += "  Avg I: " + val + "A\n"
+            leaf[pd]["I_Avg"] = val
         if TPCE_PD & 0x20:
             val,reg = to_int(t,reg,1)
             ret += "  Peak I: " + val + "A\n"
+            leaf[pd]["I_Peak"] = val
         if TPCE_PD & 0x40:
             val,reg = to_int(t,reg,1)
             ret += "  PDwn I: " + val + "A\n"
+            leaf[pd]["I_PDwn"] = val
     if TPCE_FS & 0x04:
         TPCE_TD = t[reg]
         reg+=1
-        ret += " WAIT Scale: " + str(TPCE_TD&0x03>>0) + "\n"
-        ret += " READY Scale: " + str(TPCE_TD&0x1C>>2) + "\n"
+        ret += " WAIT Scale: " + str((TPCE_TD&0x03)>>0) + "\n"
+        leaf["TD_WAIT"] = (TPCE_TD&0x03)>>0
+        ret += " READY Scale: " + str((TPCE_TD&0x1C)>>2) + "\n"
+        leaf["TD_READY"] = (TPCE_TD&0x1C)>>2
     if TPCE_FS & 0x08:
         TPCE_IO = t[reg]
         reg+=1
@@ -296,78 +367,111 @@ def CISTPL_CFTABLE_ENTRY(t):
                 3 : "8/16bit",
                 }
         ret += " " + bust[(TPCE_IO&0x60)>>5] + "\n"
+        leaf["IO_TYPE"] = bust[(TPCE_IO&0x60)>>5]
         ret += " IOAddrLines: " + str(TPCE_IO&0x1F) + "\n"
+        leaf["IO_AddrLines"] = TPCE_IO&0x1F
         if TPCE_IO & 0x80:
+            leaf["IO_RANGE"] = {}
             IO_RANGE = t[reg]
             reg+=1
             for i in range((IO_RANGE&0x0f)+1):
                 ret += " Range: " + str(i) + "\n"
+                leaf["IO_RANGE"][i] = {}
                 addr = 0
                 for n in range((IO_RANGE&0x20)>>4):
                     addr |= t[reg] << (n*8)
                     reg+=1
                 ret += "  Address: " + hex(addr) + "\n"
+                leaf["IO_RANGE"][i]["ADDR"] = addr
                 length = 0
                 for n in range((IO_RANGE&0xC0)>>6):
                     length |= t[reg] << (n*8)
                     reg+=1
                 ret += "  Length: " + hex(length+1) + "\n"
+                leaf["IO_RANGE"][i]["LEN"] = length+1
     if TPCE_FS & 0x10:
         TPCE_IRQ = t[reg]
         reg+=1
         ret += " IRQ Share=" +str(bool(TPCE_IRQ&0x80)) + "\n"
+        leaf["IRQ_Share"] = bool(TPCE_IRQ&0x80)
         ret += " IRQ Pulse=" +str(bool(TPCE_IRQ&0x40)) + "\n"
+        leaf["IRQ_Pulse"] = bool(TPCE_IRQ&0x40)
         ret += " IRQ Level=" +str(bool(TPCE_IRQ&0x20)) + "\n"
+        leaf["IRQ_Level"] = bool(TPCE_IRQ&0x20)
         if not TPCE_IRQ&0x10:
             ret += " IRQ Line=" +str(TPCE_IRQ&0x0F) + "\n"
+            leaf["IRQ_Line"] = TPCE_IRQ&0x0F
         else:
             ret += " IRQ VEND=" +str(bool(TPCE_IRQ&0x08)) + "\n"
+            leaf["IRQ_VEND"] = bool(TPCE_IRQ&0x08)
             ret += " IRQ BERR=" +str(bool(TPCE_IRQ&0x04)) + "\n"
+            leaf["IRQ_BERR"] = bool(TPCE_IRQ&0x04)
             ret += " IRQ IOCK=" +str(bool(TPCE_IRQ&0x02)) + "\n"
+            leaf["IRQ_IOCK"] = bool(TPCE_IRQ&0x02)
             ret += " IRQ NMI=" +str(bool(TPCE_IRQ&0x01)) + "\n"
+            leaf["IRQ_NMI"] = bool(TPCE_IRQ&0x01)
             mask = t[reg] | (t[reg+1]<<8)
             reg+=2
             ret += " IRQ Mask=" + hex(mask) + "\n"
+            leaf["IRQ_Mask"] = mask
     if (TPCE_FS & 0x60)>>5 == 0:
         pass
     if (TPCE_FS & 0x60)>>5 == 1:
         length = (t[reg] | (t[reg+1]<<8))*256
         reg+=2
         ret += " Memory Length: " + str(length) + "\n"
+        leaf["MEM_RANGE"] = {0:{}}
+        leaf["MEM_RANGE"][0]["Addr"] = 0
+        leaf["MEM_RANGE"][0]["Len"] = length
+        leaf["MEM_RANGE"][0]["Host_Addr"] = 0
     if (TPCE_FS & 0x60)>>5 == 2:
         length = (t[reg] | (t[reg+1]<<8))*256
         addr = (t[reg+2] | (t[reg+3]<<8))*256
         reg+=4
         ret += " Memory Address: " + hex(addr) + "\n"
         ret += " Memory Length: " + str(length) + "\n"
+        leaf["MEM_RANGE"] = {0:{}}
+        leaf["MEM_RANGE"][0]["MEM_Addr"] = addr
+        leaf["MEM_RANGE"][0]["MEM_Len"] = length
+        leaf["MEM_RANGE"][0]["Host_Addr"] = 0
     if (TPCE_FS & 0x60)>>5 == 3:
         TPCE_MS = t[reg]
         reg+=1
+        leaf["MEM_RANGE"] = {}
         for i in range((TPCE_MS&0x07)+1):
+            leaf["MEM_RANGE"][i] = {}
             ret += " Range: " + str(i) + "\n"
             length = 0
             for n in range((TPCE_MS&0x18)>>3):
                 length |= t[reg] << (n*8)
                 reg+=1
             ret += "  Length: " + hex(length*256) + "\n"
+            leaf["MEM_RANGE"][i]["MEM_Len"] = length*256          
             addr = 0
             for n in range((TPCE_MS&0x60)>>5):
                 addr |= t[reg] << (n*8)
                 reg+=1
             ret += "  Address: " + hex(addr*256) + "\n"
+            leaf["MEM_RANGE"][i]["MEM_Addr"] = addr*256
+            leaf["MEM_RANGE"][i]["Host_Addr"] = 0
             if TPCE_MS&0x80:
                 haddr = 0
                 for n in range((TPCE_MS&0x60)>>5):
                     haddr |= t[reg] << (n*8)
                     reg+=1
-                ret += "  Host Address: " + hex(addr*256) + "\n"
+                ret += "  Host Address: " + hex(haddr*256) + "\n"
+                leaf["MEM_RANGE"][i]["Host_Addr"] = haddr*256
     if TPCE_FS & 0x80:
         TPCE_MI = t[reg]
         reg+=1
         ret += " Max Twin Cards: " + str(TPCE_MI&0x7) + "\n"
+        leaf["FS_TwinCards"] = TPCE_MI&0x7
         ret += " Audio: " + str(bool(TPCE_MI&0x8)) + "\n"
+        leaf["FS_Audio"] = bool(TPCE_MI&0x8)
         ret += " Read only: " + str(bool(TPCE_MI&0x10)) + "\n"
+        leaf["FS_RO"] = bool(TPCE_MI&0x10)
         ret += " Pwr Down: " + str(bool(TPCE_MI&0x20)) + "\n"
+        leaf["FS_PWRDWN"] = bool(TPCE_MI&0x20)
         if TPCE_MI&0x80:
             TPCE_MI2 = t[reg]
             reg+=1
@@ -378,13 +482,17 @@ def CISTPL_CFTABLE_ENTRY(t):
                     3 : "INPACK",
                 }
             ret += " Dma: " + dmadict[(TPCE_MI2&0x0C)>>2] + "\n"
+            leaf["FS_DMA_PIN"] = dmadict[(TPCE_MI2&0x0C)>>2]
             ret += " Dma: " + ("16bit" if (TPCE_MI2&0x10) else "8bit") + "\n"
+            leaf["FS_DMA_Width"] = 16 if (TPCE_MI2&0x10) else 8
             if TPCE_MI2&0x80:
                 ret += " TH: " + str(t[reg]) + "." + str(t[reg+1]) + "\n"
+                leaf["FS_TH"] = str(t[reg]) + "." + str(t[reg+1])
                 reg+=2
+    pdict["CFTABLE"][idx].append(leaf)
     return ret
 
-def CISTPL_NO_LINK(t):
+def CISTPL_NO_LINK(t,pdict):
     ret = "CISTPL_NO_LINK "  + str(t) + "\n"
     return ret
 
@@ -401,14 +509,15 @@ knownid = {
         0x14 : CISTPL_NO_LINK,
         }
 
-def pprinter(t):
+def pprinter(t,pdict):
     if t[0] in knownid.keys():
-        return knownid[t[0]](t)
+        return knownid[t[0]](t,pdict)
     else:
         return str(bytes(t)) + "\n" + str(t)
 
 if __name__ == '__main__':
     f=open("attr.bin","rb")
+    parseddict = {}  
     while True:
         t=[]
         t.append(f.read(1)[0])
@@ -418,4 +527,6 @@ if __name__ == '__main__':
         t.append(l)
         for a in range(l):
             t.append(f.read(1)[0])
-        print(pprinter(t))
+        print(pprinter(t,parseddict))
+    import yaml
+    print(yaml.dump(parseddict, default_flow_style=False))
